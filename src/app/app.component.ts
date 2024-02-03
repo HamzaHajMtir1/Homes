33a03997f793d6ed05851128b6cdfb1df8ce3d0e
HamzaHajMtir1 <hamzahajmtir2002@gmail.com>
2024-02-03 17:00:05 +0100

    pdated a default Angular app to display Hello world

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h1>Hello world!</h1>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
