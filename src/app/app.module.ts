3a962705b7a107c85d20b121fc8649392086ffc8
HamzaHajMtir1 <hamzahajmtir2002@gmail.com>
2024-02-03 17:56:09 +0100

    Created a new component for your app and added it to the app's layout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HomeComponent,
+    HousingLocationComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,12 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   template: `
-    <section>
-      <form class="mt-5">
-        <input type="text" placeholder="Filter by city">
-        <button class="primary" type="button">Search</button>
-      </form>
-    </section>
+  <section>
+    <form>
+      <input type="text" placeholder="Filter by city">
+      <button class="primary" type="button">Search</button>
+    </form>
+  </section>
+  <section class="results">
+    <app-housing-location></app-housing-location>
+  </section>
   `,
   styleUrl: './home.component.css'
 })
